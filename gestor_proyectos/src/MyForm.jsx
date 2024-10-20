1de4120196ac70fbb63c5870295b909a3d99b2dc
Rodrigo Salinas <salinas.e.rodrigo@gmail.com>
2024-10-20 16:52:46 -0300

    formulario agrega nodos y aristas con el formato adecuado al estado

--- gestor_proyectos/src/MyForm.jsx
+++ gestor_proyectos/src/MyForm.jsx
@@ -1,38 +1,75 @@
-import { useState } from "react"
+import { useState } from "react";
+import './MyForm.css'
 
 export const MyForm = () => {
-  const [nodos, setNodos] = useState([])
-  //const [aristas, setAristas] = useState([])
-  
+  const [nodos, setNodos] = useState([]);
+  const [aristas, setAristas] = useState([]);
+
   const handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
+    if (nodos.find((element) => element.id == event.target.nodo.value)) {
+      return;
+    }
+    //crear nodo
     const nodo = {
       id: event.target.nodo.value,
-      label: event.target.nodo.value
-    }
+      label: event.target.nodo.value,
+    };
     const newNodos = [
       ...nodos,
       {
-        ...nodo
+        ...nodo,
       },
-    ]
-    console.log(newNodos)
-    setNodos(newNodos)
-    return
-  }
-  
-  return(
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="nodo" id="inp-nodo" />
+    ];
+    setNodos(newNodos);
+    console.log(newNodos);
+
+    //Crear aristas
+    let seleccionados = event.target.querySelectorAll(
+      'input[type="checkbox"]:checked'
+    );
+    if (seleccionados.length > 0) {
+      const newAristas = [...aristas];
+      seleccionados.forEach((element) => {
+        const arista = {
+          source: element.value,
+          target: nodo.id,
+          id: `${element.value}-${nodo.id}`,
+          label: `${element.value}-${nodo.id}`,
+        };
+        newAristas.push(arista);
+      });
+      console.log(newAristas);
+      setAristas(newAristas);
+    }
+
+    event.target.reset();
+    return;
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="add-form">
       <label htmlFor="inp-nodo">Agregar un nodo</label>
+      <input type="text" name="nodo" id="inp-nodo" />
       <div>
-        <input type="checkbox" name="equis" id="" />
-        <input type="checkbox" name="equis" id="" />
-        <input type="checkbox" name="equis" id="" />
-        <input type="checkbox" name="equis" id="" />
+        <label htmlFor="inp-arista">Agregar relación</label>
+        {nodos.length > 0 ? (
+          nodos.map((nodo, index) => (
+            <div key={`${nodo.id}-${index}`}>
+              <label htmlFor={`${nodo.id}-${index}`}>{nodo.label}</label>
+              <input
+                type="checkbox"
+                name={nodo.id}
+                id={`${nodo.id}-${index}`}
+                value={nodo.id}
+              />
+            </div>
+          ))
+        ) : (
+          <></>
+        )}
       </div>
-      <label htmlFor="inp-arista">Agregar relación</label>
       <input type="submit" value="Agregar" />
     </form>
-  )
-}
+  );
+};
