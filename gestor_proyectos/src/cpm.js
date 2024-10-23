b10fcb70938bab1663a6823952fb5ebc5625f2ae
Rodrigo Salinas <salinas.e.rodrigo@gmail.com>
2024-10-22 23:00:06 -0300

    ya se calculan correctamente los inicios y finales tanto tardios como tempranos

--- gestor_proyectos/src/cpm.js
+++ gestor_proyectos/src/cpm.js
@@ -2,14 +2,12 @@
 export const cmp = ({nodos}) => {
   console.log(nodos)
   temprano(nodos, nodos[0], 0)
-
-
+  tardio(nodos, nodos[1], nodos[1].fin_temp)
 }
 
 
 
 function temprano (nodos, nodo, fin_anter){
-  console.log(nodo, fin_anter)
   if(!nodo.int_temp){
     nodo.int_temp = fin_anter
   }else{
@@ -18,11 +16,19 @@ function temprano (nodos, nodo, fin_anter){
   nodo.fin_temp = nodo.int_temp + nodo.duracion
 
   nodo.sucesores.forEach(element => {
-    console.log(element)
     temprano(nodos, nodos[element], nodo.fin_temp)
   });
 }
 
-function tardio ({nodo, aristas}){
+function tardio (nodos, nodo, int_anterior){
+  if(!nodo.fin_tard){
+    nodo.fin_tard = int_anterior
+  }else{
+    if(int_anterior<nodo.fin_tard) nodo.fin_tard = int_anterior
+  }
+  nodo.int_tard = nodo.fin_tard - nodo.duracion 
 
+  nodo.predecesores.forEach(element => {
+    tardio(nodos, nodos[element], nodo.int_tard)
+  });
 }
