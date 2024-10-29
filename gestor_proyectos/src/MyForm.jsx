95eb7c916b9861463efe0dde021c86676299de64
Rodrigo Salinas <salinas.e.rodrigo@gmail.com>
2024-10-29 11:55:28 -0300

    se arreglo el nombre de myform.css

--- gestor_proyectos/src/MyForm.jsx
+++ gestor_proyectos/src/MyForm.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { cmp } from "./cpm";
-import "./Myform.css";
+import "./MyForm.css";
 
 export const MyForm = ({ nodos, setNodos, aristas, setAristas }) => {
   // const mystyle = {
